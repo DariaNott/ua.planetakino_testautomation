50a62a4b40fa014b02c19cea289fed1f2ac6658a
Daria Ivanova <dasha.tas97@gmail.com>
2019-09-22 00:29:38 +0300

    added native driver support

--- src/main/java/ua/planetakino/web/ui/pages/Header.java
+++ src/main/java/ua/planetakino/web/ui/pages/Header.java
@@ -4,8 +4,11 @@ import io.qameta.allure.Step;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,16 +18,16 @@ public class Header {
     private WebDriver driver;
     protected static final Logger LOGGER = Logger.getLogger(BasePage.class);
 
-    @FindBy(xpath = "//li/a[contains(@href,'showtimes')]")
+    @FindBy(linkText = "Розклад")
     private WebElement scheduleButton;
 
-    @FindBy(xpath = "//li/a[contains(@href,'movies')]")
+    @FindBy(linkText = "Фільми")
     private WebElement moviesButton;
 
     @FindBy(xpath = "//div[contains(@class,'city-selector')]")
     private WebElement cityButton;
 
-    @FindBy(xpath = "//a[contains(@href,'enter')]")
+    @FindBy(linkText = "Вхід")
     private WebElement logInButton;
 
     @FindBy(xpath = "//a[contains(@class,'logout')]")
@@ -41,14 +44,16 @@ public class Header {
 
     @Step("Clicking on 'Schedule' button in header")
     public SchedulePage goToSchedulePage() {
-        scheduleButton.click();
+        waitClickability(scheduleButton);
+        click(scheduleButton);
         LOGGER.info("Clicked on scheduleButton.");
         return new SchedulePage(driver);
     }
 
     @Step("Clicking on 'Movies' button in header")
     public MoviesPage goToMoviesPage() {
-        moviesButton.click();
+        waitClickability(moviesButton);
+        click(moviesButton);
         LOGGER.info("Clicked on moviesButton.");
         return new MoviesPage(driver);
     }
@@ -80,5 +85,16 @@ public class Header {
         cityButton.click();
         LOGGER.info("Clicked on cityButton.");
         return this;
+    }
+
+    public WebElement waitClickability(WebElement element) {
+        new WebDriverWait(driver, 40).until(ExpectedConditions.elementToBeClickable(element));
+        return element;
+    }
+
+    public void click(WebElement element) {
+        Actions actions = new Actions(driver);
+        waitClickability(element);
+        actions.moveToElement(element).click().build().perform();
     }
 }
