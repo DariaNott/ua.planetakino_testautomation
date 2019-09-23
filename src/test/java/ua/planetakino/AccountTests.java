87ef94e01a0630194e6b46c672e386b06710d819
Daria Ivanova <dasha.tas97@gmail.com>
2019-09-23 21:20:07 +0300

    test 'ceckSecretWordChange' fixed

--- src/test/java/ua/planetakino/AccountTests.java
+++ src/test/java/ua/planetakino/AccountTests.java
@@ -6,21 +6,21 @@ import ua.planetakino.entity.Account;
 public class AccountTests extends TestBase {
 
     @Test
-    public void checkLogIn () {
-        String status =  mainPage.getHeader().goToLogIn().logIn().getAuthorizationStatus();
+    public void checkLogIn() {
+        String status = mainPage.getHeader().goToLogIn().logIn().getAuthorizationStatus();
         helper.verifyAuthorizedUser(status);
     }
 
     @Test
-    public void checkLogOut () {
+    public void checkLogOut() {
         String status = mainPage.getHeader().goToLogIn().logIn().getHeader().logOut().getAuthorizationStatus();
         helper.verifyAnonymousUser(status);
     }
 
     @Test
     public void checkNameChange() {
-        String name = "Amy";
-        String surname = "Pond";
+        String name = "Робин";
+        String surname = "Шантель";
         Account account = mainPage.getHeader().goToLogIn().logIn().gotoEditAccountPage().changeName(name, surname).confirmChanges()
                 .gotoEditAccountPage().getAccountInfo();
         helper.verifyProfileChangesInName(account, name, surname);
@@ -28,7 +28,7 @@ public class AccountTests extends TestBase {
 
     @Test
     public void checkSecretWordChange() {
-        String secretWord = "timey-wimey";
+        String secretWord = "доберман";
         Account account = mainPage.getHeader().goToLogIn().logIn().gotoEditAccountPage().changeSecretWord(secretWord)
                 .confirmChanges().gotoEditAccountPage().getAccountInfo();
         helper.verifyProfileChangesInSecretWord(account, secretWord);
